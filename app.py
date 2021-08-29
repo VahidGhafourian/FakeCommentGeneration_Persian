d183a1f17650fcacd0582a3d5703786a3a6c01fe
VahidGhafourian <vahidghafourian@gmail.com>
2021-08-29 12:11:12 +0430

    Flask first api test is done.

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
-import flask_restful
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+app.logger.setLevel('INFO')
+api = Api(app)
 
-@app.route('/')
-def hello():
-    return "Hello world"
+
+# @app.route('/')
+# def hello():
+#     return "Hello world"
+class Hello(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(Hello, '/hello')
 
 if __name__ == "__main__":
     app.run(debug=True)
